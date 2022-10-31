c1d6113ad7336a99d625046aaad7397999f32b8c
joaopdg <jpgon@live.com.pt>
2022-10-31 14:47:57 +0000

    fix images

--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -21,12 +21,22 @@ const drones = [
     name: "Creeper XL 500",
     propellers: 3,
     maxSpeed: 12,
+    image:
+      "https://www.yankodesign.com/images/design_news/2022/08/auto-draft/dji_hitop_tricopter_drone_2.jpg",
   },
-  { name: "Racer 57", propellers: 4, maxSpeed: 20 },
+  {
+    name: "Racer 57",
+    propellers: 4,
+    maxSpeed: 20,
+    image:
+      "https://cdn11.bigcommerce.com/s-5v6sbv5p/images/stencil/1280x1280/products/123026/323839/700235__23151.1623237170.jpg?c=2?imbypass=on",
+  },
   {
     name: "Courier 3000i",
     propellers: 6,
     maxSpeed: 18,
+    image:
+      "https://ae01.alicdn.com/kf/HTB176aqMVXXXXbzXpXXq6xXFXXXK/F08618-F-DIY-Drone-Quadcopter-Upgraded-Full-Kit-HMF-S550-9045-3-Propeller-6-axle-Multitor.jpg",
   },
 ];
 
